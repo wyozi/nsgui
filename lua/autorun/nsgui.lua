a53b9762a18f829324ae39d33cf538d5b37a88af
Author <epicauthorjames@gmail.com>
2015-04-25 20:35:19 +0200

    Added nsgui.Example (function and command)
    For easier debugging. include can't find example.lua, neither.

--- lua/autorun/nsgui.lua
+++ lua/autorun/nsgui.lua
@@ -15,3 +15,24 @@ Add("nsgui/skinbase.lua")
 AddLUA("nsgui/traits")
 AddLUA("nsgui/skins")
 AddLUA("nsgui/comps")
+
+if SERVER then return end
+-- It can't find the file example.lua for some reason
+
+function nsgui.Example()
+	local fr = vgui.Create("NSFrame")
+	fr:SetSize(800, 400) -- 600 is way too big for my low resolution
+	fr:Center()
+	fr:SetTitle("Hello world")
+
+	local btn = vgui.Create("NSButton", fr)
+	btn:SetPos(25, 200)
+	btn:SetSize(200, 35)
+	btn:SetText("Click me")
+
+	fr:MakePopup()
+end
+
+concommand.Add("nsgui.Example", function()
+	nsgui.Example()
+end)
