bfa184ca4e2a80183cc004c98e1e39cbab36c7d2
wyozi <joonasmertanen@gmail.com>
2015-04-25 20:32:07 +0300

    Replace unneeded util function

--- lua/nsgui/skins/sleek_blur.lua
+++ lua/nsgui/skins/sleek_blur.lua
@@ -1,14 +1,14 @@
 local SKIN = {}
 
 local blur = Material("pp/blurscreen")
-function SKIN:DrawBlur(p, amount, heavyness)
-	local x, y = p:GetRelativePos(0, 0)
+function SKIN:DrawBlur(p, amount, passes)
+	local x, y = p:LocalToScreen(0, 0)
 	local scrW, scrH = ScrW(), ScrH()
 
 	surface.SetDrawColor(255,255,255)
 	surface.SetMaterial(blur)
 
-	for i = 1, heavyness do
+	for i = 1, passes do
 		blur:SetFloat("$blur", (i / 3) * (amount or 6))
 		blur:Recompute()
 
