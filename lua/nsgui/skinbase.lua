25f23e678687f52eec0d31631739a57b754b9ba1
wyozi <joonasmertanen@gmail.com>
2015-04-26 19:58:53 +0300

    Revert to default skin on inexistent skin

--- lua/nsgui/skinbase.lua
+++ lua/nsgui/skinbase.lua
@@ -1,10 +1,17 @@
 nsgui.skin = nsgui.skin or {}
 nsgui.skin.Skins = nsgui.skin.Skins or {}
 
+local DEFAULT_SKIN = "default"
+
+function nsgui.skin.Get(id)
+	id = id or DEFAULT_SKIN
+	return nsgui.skin.Skins[id]
+end
+
 function nsgui.skin.Register(id, tbl, parent)
 	-- Every skin (except default) must be inherited from default
-	if id ~= "default" and not parent then
-		parent = "default"
+	if id ~= DEFAULT_SKIN and not parent then
+		parent = DEFAULT_SKIN
 	end
 
 	if parent then
@@ -20,12 +27,23 @@ function nsgui.skin.Register(id, tbl, parent)
 	return tbl
 end
 
-local DEFAULT_SKIN = "default"
+function nsgui.skin.PanelSkin(panel)
+	local skin = panel:GetSkin() or DEFAULT_SKIN
+
+	local skinobj = nsgui.skin.Get(skin)
+	if not skinobj then
+		print("[NSGUI Warning] Skin '" .. skin .. "' not found. Reverting to 'default'")
+		panel:SetSkin("default")
+		skinobj = nsgui.skin.Get()
+	end
+
+	return skinobj
+end
+
 
 function nsgui.skin.HookPaint(metapanel, funcname, returnValue)
 	metapanel.Paint = function(panel, w, h)
-		local skin = panel:GetSkin() or DEFAULT_SKIN
-		local skinobj = nsgui.skin.Skins[skin] or nsgui.skin.Skins[DEFAULT_SKIN]
+		local skinobj = nsgui.skin.PanelSkin(panel)
 
 		skinobj[funcname](skinobj, panel, w, h)
 
@@ -33,13 +51,13 @@ function nsgui.skin.HookPaint(metapanel, funcname, returnValue)
 	end
 end
 
-function nsgui.skin.HookCall(metapanel, funcname)
-	local skin = metapanel:GetSkin() or DEFAULT_SKIN
-	local skinobj = nsgui.skin.Skins[skin] or nsgui.skin.Skins[DEFAULT_SKIN]
-	local w, h = metapanel:GetSize()
+function nsgui.skin.HookCall(panel, funcname)
+	local skinobj = nsgui.skin.PanelSkin(panel)
+		
+	local w, h = panel:GetSize()
 
 	if skinobj[funcname] then
-		return skinobj[funcname](metapanel, metapanel, w, h)
+		return skinobj[funcname](panel, panel, w, h)
 	end
 	
 	return false
