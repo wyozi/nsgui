d8d56254919133d71e4b061ecbfa289721bc3d9c
wyozi <joonasmertanen@gmail.com>
2015-04-26 20:06:07 +0300

    Fix textentry having duplicated text accessors

--- lua/nsgui/comps/textentry.v01.lua
+++ lua/nsgui/comps/textentry.v01.lua
@@ -1,6 +1,5 @@
 local PANEL = {}
 
-AccessorFunc(PANEL, "_text", "Text")
 AccessorFunc(PANEL, "_textcolor", "TextColor")
 AccessorFunc(PANEL, "_color", "Color")
 AccessorFunc(PANEL, "_font", "Font")
