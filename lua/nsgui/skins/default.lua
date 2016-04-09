7ac47089a2ac3403265c873c608e7db42a2ddf39
Author <author@glua.me>
2016-04-09 14:15:12 +0200

    Dumb mistake

--- lua/nsgui/skins/default.lua
+++ lua/nsgui/skins/default.lua
@@ -1,10 +1,6 @@
 local SKIN = {}
 
 --
--- Made by Author.
---
-
---
 -- Fonts
 --
 
@@ -12,6 +8,7 @@ surface.CreateFont("Roboto 16", {
 	size = 16,
 	font = "Roboto",
 })
+
 surface.CreateFont("Roboto 24", {
 	size = 24,
 	font = "Roboto",
@@ -66,12 +63,23 @@ function SKIN:PaintFrame(panel, w, h)
 	self:PaintFrameSizableHandle(panel, w-0, h-0)
 end
 
-	--
-	-- Main Frame Button
-	--
+SKIN.Color_FrameSizableHandle = Color(127, 127, 127)
+
+function SKIN:PaintFrameSizableHandle(panel, w, h)
+	surface.SetDrawColor(self.Color_FrameSizableHandle)
+
+	local padding = 3
+	surface.DrawLine(w-padding, h-6, w-6, h-padding)
+	surface.DrawLine(w-padding, h-10, w-10, h-padding)
+	surface.DrawLine(w-padding, h-14, w-14, h-padding)
+end
+
+--
+-- Main Frame Button
+--
 
 function SKIN:PaintFrameCloseButton(panel, w, h)
-	surface.SetDrawColor(255,100,100, panel.Hovered and 255 or 0)
+	surface.SetDrawColor(255, 75, 75, panel.Hovered and 255 or 0)
 	surface.DrawRect(0,0,w,h)
 	
 	surface.SetTextColor ( 255, 255, 255 )
@@ -114,6 +122,89 @@ function SKIN:PaintButton(panel, w, h)
 	surface.DrawOutlinedRect(0, 0, w, h)
 
 	draw.SimpleText(panel:GetText(), panel:GetFont() or self.Font, w/2, h/2, fgclr, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+end
+
+--
+-- Label
+--
+
+SKIN.Color_LabelForeground = Color(51, 51, 51)
+
+function SKIN:GetAlignedLabelPos(panel, tw, th, w, h)
+	local alignment = panel:GetContentAlignment()
+
+	local midx, midy = w/2 - tw/2, h/2 - th/2
+	local left, top = 0, 0
+	local right, bot = w - tw, h - th
+
+	if alignment == 7 then -- NW
+		return left, top
+	elseif alignment == 8 then -- N
+		return midx, top
+	elseif alignment == 9 then -- NE
+		return right, top
+	elseif alignment == 4 then -- W
+		return left, midy
+	elseif alignment == 5 then -- CENTER
+		return midx, midy
+	elseif alignment == 6 then -- E
+		return right, midy
+	elseif alignment == 1 then -- SW
+		return left, bot
+	elseif alignment == 2 then -- S
+		return midx, bot
+	elseif alignment == 3 then -- SE
+		return right, bot
+	end
+
+	-- Invalid alignment. TODO should we error here?
+	return midx, midy
+end
+
+function SKIN:PaintLabel(panel, w, h)
+	local fgclr = panel:GetTextColor() or self.Color_LabelForeground
+	local font = panel:GetFont() or self.Font
+
+	surface.SetFont(font)
+	local tw, th = surface.GetTextSize(panel:GetText())
+	local x, y = self:GetAlignedLabelPos(panel, tw, th, w, h)
+
+	draw.SimpleText(panel:GetText(), font, x, y, fgclr)
+end
+
+--
+-- TextEntry
+--
+
+SKIN.Color_TextEntryBackground = Color(236, 236, 236)
+SKIN.Color_TextEntryOutline = Color(0, 0, 0, 100)
+SKIN.Color_TextEntryForeground = Color(51, 51, 51)
+SKIN.Color_TextEntryForegroundDisabled = Color(191, 191, 191)
+SKIN.Color_TextEntryForegroundHighlighted = Color(107, 185, 240)
+SKIN.Font_TextEntry = SKIN.Font
+
+function SKIN:PaintTextEntryBackground(panel, w, h)
+	surface.SetDrawColor(self.Color_TextEntryBackground)
+	surface.DrawRect(0, 0, w, h)
+
+	surface.SetDrawColor(self.Color_TextEntryOutline)
+	surface.DrawOutlinedRect(0, 0, w, h)
+end
+function SKIN:PaintTextEntry(panel, w, h)
+	self:PaintTextEntryBackground(panel, w, h)
+
+	-- We use Source function to draw text, so we need to do this to set
+	-- the font to the one we want. Set/GetFont is overridden in textentry.lua
+	-- so this is okay
+	panel:SetFontInternal(panel:GetFont() or self.Font_TextEntry)
+
+	local color = panel:GetTextColor() or self.Color_TextEntryForeground
+
+	if not panel:IsEnabled() then
+		color = self.Color_TextEntryForegroundDisabled
+	end
+
+	panel:DrawTextEntryText(color, self.Color_TextEntryForegroundHighlighted, color)
 end
 
 --
